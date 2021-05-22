a72d33e3e1e384f9e1ca4e56a133efacfd1d9a97
Hturoy <69842773+Hturoy@users.noreply.github.com>
2021-05-22 16:47:30 +0200

    qqq

--- contact.js
+++ contact.js
@@ -20,7 +20,7 @@ function validate(r){
     console.log("Subject: " + nameSub);
     nameMsg.innerHTML = "";
     if (nameSub.length < 5) {
-        nameMsg.innerHTML += "Must be at least 5 characters!"
+        nameMsg.innerHTML += "Du må ha minst 5 karakterer"
     }
     
     // Validate email
@@ -29,21 +29,21 @@ function validate(r){
       emailMsg.innerHTML = ""; 
       let emailTest = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       if (!emailTest.test(emailSub)) {
-        emailMsg.innerHTML += "This is not a valid E-mail address";
+        emailMsg.innerHTML += "Vennligst tast inn en gyldig E-post addresse";
       }
     //validate Subject
     let subjectSub = subject.value.trim();
     console.log("Subject: " + subjectSub);
     subjectMsg.innerHTML = "";
         if (subjectSub.length < 15) {
-        subjectMsg.innerHTML += "Must be at least 15 characters!"
+        subjectMsg.innerHTML += "Du må ha minst 15 karakterer"
 }
     // validate message
       let messageSub = message.value.trim();
     console.log("Message: " + messageSub);
     messageMsg.innerHTML = "";
     if (messageSub.length < 25) {
-        messageMsg.innerHTML += "Must be at least 25 characters!"
+        messageMsg.innerHTML += "Du må ha minst 25 karakterer"
     }
     }
 
