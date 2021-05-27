62e93bd45ef2b663bd35574dba8eff83fe6809cb
Hturoy <69842773+Hturoy@users.noreply.github.com>
2021-05-27 20:31:09 +0200

    arbeid

--- blogPost.js
+++ blogPost.js
@@ -4,6 +4,9 @@ const para = new URLSearchParams (getUrl)
 const id = para.get("id")
 const url = `https://henrikturoy.no/wp-json/wp/v2/posts/${id}?_embed=true`
 const postContent = document.querySelector(".postContent")
+const postImg = document.querySelector(".popOut")
+const postImgOpen =  document.querySelector(".postImgOpen")
+const body = document.querySelector(".bodyx")
 
 
  listPost = (post) => {
@@ -12,13 +15,26 @@ const postContent = document.querySelector(".postContent")
     console.log(post)
     let newContent = `
     
-        <h2>${post.title.rendered}</h2>
-        <img class="postImg" src="${post._embedded["wp:featuredmedia"][0].source_url}"></img>
+        <h1>${post.title.rendered}</h1>
         <p> ${post.content.rendered}</p>  
    
     `
+    let imgContent = `
+    <img class="postImg" src="${post._embedded["wp:featuredmedia"][0].source_url}"></img>
+    `
     postContent.innerHTML += newContent;
-    
+    postImg.innerHTML += imgContent;
+
+    postImg.addEventListener("click", function(){
+      postImg.classList.toggle("postImgOpen")
+    })
+
+
+    // window.onclick = function(event){
+    //   if (event.target == postImg){
+    //     postImg.style.display = "none"
+    //   }
+    // }
 }
 
 fetch(url, {
